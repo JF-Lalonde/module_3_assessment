aecb5f56df47d11ffba46cf0c8371e9dd6639add
JF-Lalonde <lalondej88@gmail.com>
2017-09-13 11:18:20 -0600

    PORO built out, need to specify params for zipcode

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,5 +1,22 @@
 class Store
+  attr_reader :name,
+              :city,
+              :distance,
+              :phone_number,
+              :store_type
+
+  def initialize(attributes = {})
+    @name = attributes[:longName]
+    @city = attributes[:city]
+    @distance = attributes[:distance]
+    @phone_number = attributes[:phone]
+    @store_type = attributes[:storeType]
+  end
+
   def self.find_stores(zipcode)
-    StoreService.return_stores(zipcode)
+    stores = StoreService.new(zipcode).return_stores
+    stores.map do |store|
+      Store.new(store)
+    end
   end
 end
--- app/services/store_service.rb
+++ app/services/store_service.rb
@@ -7,7 +7,6 @@ class StoreService
   def return_stores
     response = @conn.get("/v1/stores(area(#{@zipcode},25))?format=json&show=longName,city,distance,phone,storeType&pageSize=10&apiKey=#{ENV["API_KEY"]}")
     stores = JSON.parse(response.body, symbolize_names: true)[:stores]
-    byebug
   end
 
 end
