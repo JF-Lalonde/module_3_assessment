f07cc1fdc5960b1cfe373ac77bca2dc45117b031
JF-Lalonde <lalondej88@gmail.com>
2017-09-13 09:29:41 -0600

    Second api test passing for item show request

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -3,4 +3,8 @@ class Api::V1::ItemsController < ApplicationController
     render json: Item.all
     head 200
   end
+
+  def show
+    render json: Item.find(params[:id]), except: [:created_at, :updated_at]
+  end
 end
--- spec/requests/user_can_request_items_spec.rb
+++ spec/requests/user_can_request_items_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
+describe "When a user makes an API call" do
 #When I send a GET request to `/api/v1/items`
 #I receive a 200 JSON response containing all items
 #And each item has an id, name, description, and image_url but not the created_at or updated_at
-describe "When a user makes an API call" do
   it "returns multiple items" do
     items = create_list(:item, 3)
     item = items.first
@@ -12,6 +12,20 @@ describe "When a user makes an API call" do
 
     expect(response).to be_success
     expect(items.count).to eq(3)
+    expect(item.name).to eq("SomeItem")
+    expect(item.description).to eq("It itemizes really well")
+    expect(item.image_url).to eq("photoshop.com")
+  end
+
+  #When I send a GET request to `/api/v1/items/1`
+  #I receive a 200 JSON response containing the id, name,
+  #description, and image_url but not the created_at or updated_at
+  it "returns a single item" do
+    item = create(:item)
+
+    get "/api/v1/items/#{item.id}"
+
+    expect(response).to be_success
     expect(item.name).to eq("SomeItem")
     expect(item.description).to eq("It itemizes really well")
     expect(item.image_url).to eq("photoshop.com")
