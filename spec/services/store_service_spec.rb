3f37901d34b8682f0896793eb237240292e171fb
JF-Lalonde <lalondej88@gmail.com>
2017-09-13 11:30:32 -0600

    Trying to capture search params to pass zipcode into api call

--- spec/services/store_service_spec.rb
+++ spec/services/store_service_spec.rb
@@ -6,6 +6,10 @@ describe StoreService do
     stores = StoreService.new("80202").return_stores
 
     expect(stores.count).to eq(10)
-
+    expect(stores[0]).to have_key(:longName)
+    expect(stores[0]).to have_key(:city)
+    expect(stores[0]).to have_key(:distance)
+    expect(stores[0]).to have_key(:phone)
+    expect(stores[0]).to have_key(:storeType)
   end
 end
