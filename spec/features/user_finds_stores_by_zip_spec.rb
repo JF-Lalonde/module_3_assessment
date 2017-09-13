6cd997d5bcd87c7a6273d9e4d6d7d1da130bcaa0
JF-Lalonde <lalondej88@gmail.com>
2017-09-13 10:09:14 -0600

    Add search bar to navbar

--- spec/features/user_finds_stores_by_zip_spec.rb
+++ spec/features/user_finds_stores_by_zip_spec.rb
@@ -6,7 +6,9 @@ require "rails_helper"
 #Then my current path should be "/search" (ignoring params)
 #And I should see stores within 25 miles of 80202
 #And I should see a message that says "17 Total Stores"
-#And I should see exactly 10 results (There are 17 stores within 25 miles. We want to display 10 on this page and 7 on the next. Get the first page to work first from start to finish and worry about pagination later.)
+#And I should see exactly 10 results (There are 17 stores within 25 miles.
+#We want to display 10 on this page and 7 on the next.
+#Get the first page to work first from start to finish and worry about pagination later.)
 #And I should see the long name, city, distance, phone number and store type for each of the 10 results
 
 feature "User can find BestBuy stores" do
@@ -14,5 +16,14 @@ feature "User can find BestBuy stores" do
     visit "/"
     fill_in "search_bar", with: "80202"
     click_button "Search"
+
+    expect(current_path).to eq("/search")
+    expect(page).to have_content("17 Total Stores")
+    expect(page).to have_css(".stores", count: 10)
+    expect(page).to have_css(".long_name")
+    expect(page).to have_css(".city")
+    expect(page).to have_css(".distance")
+    expect(page).to have_css(".phone_number")
+    expect(page).to have_css(".story_type")
   end
 end
